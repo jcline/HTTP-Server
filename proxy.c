567352326149f3061c9496dbc529da54fc1f6c6e
James Cline <james.cline@gatech.edu>
2011-12-06 19:23:44 -0500

    remove socket/shared mem arg

--- proxy.c
+++ proxy.c
@@ -4,22 +4,14 @@ size_t MAX_PROXY_THREADS = 10;
 
 int main(int argc, char** argv) {
 	int port;
-	size_t shared_memory;
 
-	if( argc < 4 ) {
+	if( argc < 3 ) {
 		fprintf(stderr, "Not enough arguments!\n");
 		exit(1);
 	}
 
 	port = atoi(argv[1]);
 	MAX_PROXY_THREADS = atoi(argv[2]);
-	shared_memory = ( !strcmp(argv[3], "shared") ) ? 1 : 0;
-	if(shared_memory) {
-		printf("Running in shared memory mode\n");
-	} 
-	else {
-		printf("Running in socket mode\n");
-	}
 
 	struct sigaction sa;
 	sa.sa_handler = SIG_IGN;
