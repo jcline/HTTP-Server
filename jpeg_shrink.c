bacba5d0d4260ce9fa59c5a3f9cc14b743b96217
James Cline <james.cline@gatech.edu>
2011-12-09 14:36:52 -0500

    fucking stupid

--- jpeg_shrink.c
+++ jpeg_shrink.c
@@ -7,16 +7,16 @@ img_t * shrink_img_1_svc(img_t * arg, struct svc_req * sr) {
 
 	FILE* f = fopen("scratch.jpg", "w+");
 
-	static img_t * i = (img_t*) malloc(sizeof(img_t));
+	static img_t i;
 
 	fwrite(arg->data, sizeof(char), arg->size, f);
 
 	memset(arg->data, 0, arg->size);
 
-	i->size = 100;
-	i->data = (char *)malloc(sizeof(char) *( 100 * 100 + 50 ));
+	i.size = 100;
+	i.data = (char *)malloc(sizeof(char) *( 100 * 100 + 50 ));
 
-	change_res_JPEG_F(f, &i->data, &i->size);
+	change_res_JPEG_F(f, &i.data, &i.size);
 
 	return &i;
 }
